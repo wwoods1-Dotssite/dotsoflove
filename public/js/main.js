b21607d1cb92171ca978b3a2a4a82f16aefada58
William Woods <wwoods1@gmail.com>
2025-10-28 16:51:56 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     initializeNavigation();
     loadInitialContent();
     setupDateValidation();
-    initializeEditStoryModal();
+    // initializeEditStoryModal();
 });
 
 function initializeNavigation() {
